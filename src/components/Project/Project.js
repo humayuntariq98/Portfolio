474ec9bba5f38321cae3c9439cca1de29a4b5892
humayuntariq98 <hammytar23@gmail.com>
2023-10-15 06:15:50 -0400

    update project images

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,36 +1,40 @@
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
-    Button,
-  } from "@material-tailwind/react";
-   
-  export default function Project({name, image, git, live}) {
-    return (
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+
+export default function Project({ name, image, git, live, description }) {
+  return (
     <div className="flex-row mx-auto p-10">
       <Card className="mt-6 w-96">
         <CardHeader color="blue-gray" className="relative h-56">
           <img
-            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-            alt="card-image"
+            src={image}
+            alt="card"
+            style={{ height: "250px", width: "500px" }}
           />
         </CardHeader>
         <CardBody>
           <Typography variant="h5" color="blue-gray" className="mb-2">
             {name}
           </Typography>
-          <Typography>
-            The place is close to Barceloneta Beach and bus stop just 2 min by
-            walk and near to &quot;Naviglio&quot; where you can enjoy the main
-            night life in Barcelona.
-          </Typography>
+          <Typography>{description}</Typography>
         </CardBody>
         <CardFooter className="pt-0">
-          <Button>Read More</Button>
+          <Button onClick={() => window.location.assign(live)}>Preview</Button>
+          <Button
+            ripple={true}
+            style={{ marginLeft: "10px" }}
+            onClick={() => window.location.assign(git)}
+          >
+            View Source
+          </Button>
         </CardFooter>
       </Card>
-      </div>
-    );
-  }
+    </div>
+  );
+}
